492b004f26a66437e93f840207bcf71d46e017a6
Владимир <gaiduk_vladimir@mail.ru>
2020-12-30 10:29:37 +0300

    Стабильный вариант version 0.1.0

--- src/main/java/ru/appline/framework/utils/Product.java
+++ src/main/java/ru/appline/framework/utils/Product.java
@@ -1,5 +1,7 @@
 package ru.appline.framework.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -8,6 +10,7 @@ import java.util.Objects;
 public class Product implements Comparable<Product>{
     private String name;
     private int price;
+    public static List<Product> listProducts = new ArrayList<Product>();
 
     public Product(String name, int price) {
         this.name = name;
@@ -38,5 +41,11 @@ public class Product implements Comparable<Product>{
     @Override
     public int compareTo(Product o) {
         return this.getPrice() - o.getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return  name + ", цена: " +
+                price + " ₽\n";
     }
 }
